c4cadce30f23574bb5b30d40bb013d9e9881901e
Daniel <124894477+Ridgeld@users.noreply.github.com>
2023-09-25 22:58:39 +0600

    Update S_accept_invite.lua

--- city_mayor/S_accept_invite.lua
+++ city_mayor/S_accept_invite.lua
@@ -1,6 +1,8 @@
 addEvent( "acceptInvite", true )
 addEventHandler( "acceptInvite", resourceRoot, function( player, fraction_id )
-    if getElementData( client, "fraction" ) == "city_mayor" and getElementData( client, "rang" ) == 2  then 
+    local client_fraction = getElementData( client, "fraction" )
+    local client_rang = getElementData( client, "rang" )
+    if client_fraction == "city_mayor" and client_rang == 2  then 
         outputChatBox( "Вы приняли приглашение!", player, 0, 255, 0)
         setElementData( player, "rang", 1 )
         setElementData( player, "fraction", fraction_id )
