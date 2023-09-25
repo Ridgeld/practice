5e11ec68d0332bb353ad6ee0053f141b8bb3fb04
Daniel <124894477+Ridgeld@users.noreply.github.com>
2023-09-25 22:55:45 +0600

    Update S_invite_to_fraction_show_window.lua

--- city_mayor/S_invite_to_fraction_show_window.lua
+++ city_mayor/S_invite_to_fraction_show_window.lua
@@ -2,29 +2,28 @@ local lastMessageTime = {}
 local show_invite = true
 addEvent( "inviteToJob", true )
 addEventHandler( "inviteToJob", resourceRoot, function( id )
-    if getElementData( client, "fraction" ) then 
-        local player = getPlayerByID( id )
-        if not player then
-            outputChatBox("Игрок с указанным ID не найден.",client, 255, 0, 0)
-            return
-        end
-        local currentTime = getTickCount() -- Текущее время в миллисекундах
-        local lastTime = lastMessageTime[ id ] or 0 -- Время последней отправки сообщения для данного ID
+    local client_fraction = getElementData( client, "fraction" )
+    if not client_fraction then 
+        return
+    end  
 
-        -- Проверяем, прошла ли минута с момента последней отправки для данного ID
-        if currentTime - lastTime < 60000 then
-            outputChatBox( "Вы можете отправить приглашение этому игроку только раз в 1 минуту.", client,  255, 0, 0 )
-            return
-        end
+    local player = getPlayerByID( id )
+    if not player then
+        outputChatBox("Игрок с указанным ID не найден.",client, 255, 0, 0)
+        return
+    end
+    
+    local currentTime = getTickCount()
+    local lastTime = lastMessageTime[ id ] or 0
+    if currentTime - lastTime < 60000 then
+        outputChatBox( "Вы можете отправить приглашение этому игроку только раз в 1 минуту.", client,  255, 0, 0 )
+        return
+    end
 
-        -- Обновляем время последней отправки сообщения для данного ID
-        lastMessageTime[ id ] = currentTime
-                
-        if getElementData( player, "fraction" ) then 
-            outputChatBox( "Данный игрок уже состоит во фракции!", client, 255, 0, 0 )
-                
-        else
-            triggerClientEvent( player, "inviteWindow", player,  getPlayerName( client ), getElementData( client, "fraction" ) )
-        end
+    lastMessageTime[ id ] = currentTime
+          
+    local player_fraction = getElementData( player, "fraction" )
+    if not player_fraction then 
+        triggerClientEvent( player, "inviteWindow", player,  getPlayerName( client ), getElementData( client, "fraction" ) )
     end
 end )
