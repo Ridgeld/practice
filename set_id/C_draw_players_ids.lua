0efb1f38a5ad4ee60c6e771eb1e06f14a393d790
Daniel <124894477+Ridgeld@users.noreply.github.com>
2023-09-25 22:53:24 +0600

    Update C_draw_players_ids.lua

--- set_id/C_draw_players_ids.lua
+++ set_id/C_draw_players_ids.lua
@@ -10,22 +10,13 @@ addEventHandler ( "onClientResourceStart", resourceRoot, drawPlayersIds )
 
 
 
-function getPlayersInScreenRange( player )
-    local x, y, z = getElementPosition( player ) -- Получаем позицию игрока
-    local screenWidth, screenHeight = guiGetScreenSize() -- Получаем размер экрана
+function getPlayersInRange( player )
+    local x, y, z = getElementPosition( player )
 
-    local players_in_range = {} -- Создаем таблицу для хранения игроков в радиусе
+    local players_in_range = getElementsWithinRange( x, y, z, 10, "player")
 
-    for _, other_player in ipairs( getElementsByType( "player" ) ) do
-        if other_player ~= player then
-            local px, py, pz = getElementPosition( other_player ) -- Получаем позицию другого игрока
-            local screenX, screenY = getScreenFromWorldPosition( px, py, pz + 1 ) -- Преобразуем мировые координаты в координаты на экране
-            -- Проверяем, находится ли игрок на экране и в радиусе 5 метров
-            if screenX and screenY and getDistanceBetweenPoints3D( x, y, z, px, py, pz ) <= 5 then
-                table.insert( players_in_range, other_player ) -- Добавляем игрока в таблицу
-            end
-        end 
+    for _, player in pairs( players_in_range ) do
+        return players_in_range
     end
-    return players_in_range
 end
 
