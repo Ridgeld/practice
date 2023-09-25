9ccf1464562beec9a0c2f37a85c018e598f38c7a
Daniel <124894477+Ridgeld@users.noreply.github.com>
2023-09-25 23:00:11 +0600

    Create S_create_fraction.lua

---
+++ create_fraction/S_create_fraction.lua
@@ -0,0 +1,7 @@
+function CreateFraction( fraction_id, lider_name, fraction_players )
+    FRACTION = {
+        fraction_id = fraction_id,
+        lider_name = lider_name,
+        fraction_players = fraction_players
+    }
+end
